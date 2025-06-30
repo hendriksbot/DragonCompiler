5ec238c93d4de4268ed5d907fc2a189faa105a8f
hendrik <hendrik_boettcher@yahoo.de>
2025-07-01 01:04:24 +0200

    add test case to db validation

--- validation/run_db_validation.py
+++ validation/run_db_validation.py
@@ -1,4 +1,5 @@
 """this script support a fast analysis and validation of the test db"""
+import sys
 import sqlite3
 from pathlib import Path
 
@@ -10,6 +11,27 @@ def main():
 
     print_number_of_spells(cursor)
     print_rows_of_db(cursor)
+
+    is_valid_db = is_valid_number_of_spells(cursor)
+
+    if is_valid_db:
+        print("\n✅ spell database is valid")
+    else:
+        print("\n❌ spell database is invalid")
+        sys.exit(1)
+
+
+def is_valid_number_of_spells(cursor: sqlite3.Cursor) -> bool:
+    count = cursor.execute("SELECT COUNT(*) FROM spells").fetchone()[0]
+
+    if count == 1:
+        print("✅ spell database contains the correct number of spells")
+        return True
+
+    print("❌ incorrect number of spells in in the databse")
+    print(f"  - expected 1 spell but found {str(count)} spells." )
+    return False
+
 
 def print_number_of_spells(cursor: sqlite3.Cursor):
     count = cursor.execute("SELECT COUNT(*) FROM spells").fetchone()[0]
