c1a136823f8d3a82df1b4b73c1e60ea87830fd00
hendrik <hendrik_boettcher@yahoo.de>
2025-07-01 09:07:39 +0200

    add cli help description and dummy release function

--- src/dragon_compiler/cli.py
+++ src/dragon_compiler/cli.py
@@ -6,21 +6,39 @@ from dragon_compiler import builder
 class CompilerCLI:
     """this is the command line interface class for the compiler"""
     def __init__(self):
-        self._app = typer.Typer()
-        self._app.command()(self._make_build_command())
+        self._app = typer.Typer(help="Awesome dragon compiler " \
+            "for your spells and monster database")
+        self._app.command("build")(self._make_build_command())
+        self._app.command("release")(self._make_release_command())
 
     def run(self):
         self._app()
 
     def _make_build_command(self):
         def build_command(
-            source: str = typer.Option(...,"--source", "-s"),
-            out: str = typer.Option("./build","--out", "-o")
+            source: str = typer.Option(
+                ...,"--source", "-s",
+                help ="source folder that contains the database that needs " \
+                "to be compiled"),
+            out: str = typer.Option(
+                "./build","--out", "-o",
+                help = "optional output folder where the database will be"
+                " created"),
+            do_clean: bool = typer.Option(
+                False, "--clean", "-c", is_flag=True,
+                help = "removes old build from output folder")
         ):
-            return self.build(source, out)
+            return self.build(source, out, do_clean)
         return build_command
 
-    def build(self, source: str, out: str):
+    def _make_release_command(self):
+        def release_command(
+                source: str = typer.Option(...,"--source", "-s")
+        ):
+            return self.release(source)
+        return release_command
+
+    def build(self, source: str, out: str, do_clean: bool): #pylint: disable=unused-argument
         print("ready to build")
         print("source: " + source)
         print("out: " + out)
@@ -30,6 +48,8 @@ class CompilerCLI:
         ))
         db_builder.build()
 
+    def release(self, source: str):
+        pass
 
 # Entry Point
 def main():
