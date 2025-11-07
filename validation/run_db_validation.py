a3afe63e1472941425c7845ec1f21d724e0d299d
Hendrik <hendrik_boettcher@yahoo.de>
2025-11-07 14:56:28 +0100

    enable validation script to check non spell dbs

--- validation/run_db_validation.py
+++ validation/run_db_validation.py
@@ -17,38 +17,39 @@ def main(
     db_path = Path(path_to_db)
     con = sqlite3.connect(db_path)
     cursor = con.cursor()
+    db_name = db_path.stem
 
-    print_number_of_spells(cursor)
-    print_rows_of_db(cursor)
+    print_number_of_entries(cursor, db_name)
+    print_rows_of_db(cursor, db_name)
 
-    is_valid_db = is_valid_number_of_spells(cursor)
+    is_valid_db = is_valid_number_of_entries(cursor, db_name)
 
     if is_valid_db:
-        print("\n✅ spell database is valid")
+        print("\n✅ database is valid")
     else:
-        print("\n❌ spell database is invalid")
+        print("\n❌ database is invalid")
         sys.exit(1)
 
 
-def is_valid_number_of_spells(cursor: sqlite3.Cursor) -> bool:
-    count = cursor.execute("SELECT COUNT(*) FROM spells").fetchone()[0]
+def is_valid_number_of_entries(cursor: sqlite3.Cursor, db_name: str) -> bool:
+    count = cursor.execute(f"SELECT COUNT(*) FROM {db_name}").fetchone()[0]
 
     if count == 1:
         print("✅ spell database contains the correct number of spells")
         return True
 
-    print("❌ incorrect number of spells in in the databse")
-    print(f"  - expected 1 spell but found {str(count)} spells.")
+    print("❌ incorrect number of entries in in the database")
+    print(f"  - expected 1 entry but found {str(count)} entries.")
     return False
 
 
-def print_number_of_spells(cursor: sqlite3.Cursor):
-    count = cursor.execute("SELECT COUNT(*) FROM spells").fetchone()[0]
-    print("Spells in table:", count)
+def print_number_of_entries(cursor: sqlite3.Cursor, db_name: str):
+    count = cursor.execute(f"SELECT COUNT(*) FROM {db_name}").fetchone()[0]
+    print("Entries in table:", count)
 
 
-def print_rows_of_db(cursor: sqlite3.Cursor):
-    data = cursor.execute("SELECT * FROM spells")
+def print_rows_of_db(cursor: sqlite3.Cursor, db_name: str):
+    data = cursor.execute(f"SELECT * FROM {db_name}")
     for row in data:
         print(row)
 
